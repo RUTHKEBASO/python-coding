7ec15d88d775cfacf03084d0c54408dad3768954
RUTHKEBASO <123358730+RUTHKEBASO@users.noreply.github.com>
2023-01-30 10:26:53 +0300

    Add files via upload
    how to calculate grosspay using python

---
+++ example 1.py
@@ -0,0 +1,8 @@
+def gross_pay(hour.rate):
+    return(hour*rate)
+
+    hours_worked=int(input('enter hours:'))
+    rate_per_hour=float(input('enter rate:'))
+    gross_pay=hours_worked*rate_per_hour
+
+    print('gross_pay is',gross-pay)
