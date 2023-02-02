391042c2031010079ecb0faca7faffd7b89fcd91
RUTHKEBASO <123358730+RUTHKEBASO@users.noreply.github.com>
2023-02-02 23:43:35 +0300

    Add files via upload
    checking whether a person is eligible to vote and if he or she is a Kenyan

---
+++ IEBC VOTES.py
@@ -0,0 +1,7 @@
+#kenyan election 
+nationality=(input("enter nationality:"))
+age=int(input("enter age:"))
+if age>=18 and nationality=="kenyan":
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
