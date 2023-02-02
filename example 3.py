ec85cff35f2941139ce720ecab267c77addb960a
RUTHKEBASO <123358730+RUTHKEBASO@users.noreply.github.com>
2023-02-02 22:59:26 +0300

    Add files via upload
    A code allowing a citizen to borrow a loan if he or she qualifies certain specifications.

---
+++ example 3.py
@@ -0,0 +1,7 @@
+#loan 
+age =int(input("enter age:"))
+annual_income =int(input("enter annual income:"))
+if age>=21 and annual_income>=21000:
+    print("congratulation you qualify for a loan")
+else:
+    print("unfortunately we are unable to offer you  loan at this time")
